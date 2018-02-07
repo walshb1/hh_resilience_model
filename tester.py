2138bbe174c7fe20886431031812f76d3cd5f1cc
Brian Walsh <brian.j.walsh.1@gmail.com>
2018-02-06 17:23:18 -0700

    cleanup

--- tester.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from libraries.lib_gather_data import get_hh_savings
-
-df = pd.read_csv('../../debug/temp_init.csv')
-
-print(get_hh_savings(df[['province','ispoor','axfin','c']],'PH','','../inputs/PH/Socioeconomic Resilience (Provincial)_Print Version_rev1.xlsx').head())
