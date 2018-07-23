cab5b4e341357a9fcca17341f237231e6a291296
Brian Walsh <brian.j.walsh.1@gmail.com>
2018-07-23 13:47:43 -0600

    moving optimize_libs directory up one level (out of model & github)

--- gather_data.py
+++ gather_data.py
@@ -400,10 +400,10 @@ print('Running hh_reco_rate optimization')
 hazard_ratios['hh_reco_rate'] = 0
 
 try: 
-    v_to_reco_rate = pickle.load(open('optimization_libs/'+myCountry+'_v_to_reco_rate.p','rb'))
-    pickle.dump(v_to_reco_rate, open('optimization_libs/'+myCountry+'_v_to_reco_rate_proto2.p', 'wb'),protocol=2)
+    v_to_reco_rate = pickle.load(open('../optimization_libs/'+myCountry+'_v_to_reco_rate.p','rb'))
+    pickle.dump(v_to_reco_rate, open('../optimization_libs/'+myCountry+'_v_to_reco_rate_proto2.p', 'wb'),protocol=2)
 except:
-    print('Was not able to load v to hh_reco_rate library from optimization_libs/'+myCountry+'_v_to_reco_rate.p')
+    print('Was not able to load v to hh_reco_rate library from ../optimization_libs/'+myCountry+'_v_to_reco_rate.p')
     v_to_reco_rate = {}
 
 try: hazard_ratios['hh_reco_rate'] = hazard_ratios.apply(lambda x:v_to_reco_rate[round(x.v,2)],axis=1)
@@ -419,7 +419,7 @@ except:
             hazard_ratios.loc[_i,'hh_reco_rate'] = _opt
             v_to_reco_rate[_v] = _opt
 
-    pickle.dump(v_to_reco_rate, open('optimization_libs/'+myCountry+'_v_to_reco_rate.p', 'wb' ) )
+    pickle.dump(v_to_reco_rate, open('../optimization_libs/'+myCountry+'_v_to_reco_rate.p', 'wb' ) )
 
 while True:
     _path = '/Users/brian/Desktop/Dropbox/Bank/unbreakable_writeup/Figures/'
