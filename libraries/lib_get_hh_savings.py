9645518d583608445271a32e6beb006943758911
Eugene Tan Perk Han <eugene574tan@gmail.com>
2019-03-19 12:42:51 -0400

    missed stage

--- libraries/lib_get_hh_savings.py
+++ libraries/lib_get_hh_savings.py
@@ -8,18 +8,12 @@ def get_hh_savings(myC, econ_unit, pol, fstr=None):
     elif pol == '_nosavingsdata': return hh_df.eval('c/12').to_frame(name='precautionary_savings')
     elif pol == '_infsavings': return hh_df.eval('1.E9').to_frame(name='precautionary_savings')
 
-<<<<<<< HEAD
-    # Now run country-dependent options: 
-    if myC == 'SL' or myC == 'MW': return hh_df.eval('c/12.').to_frame(name='precautionary_savings')
+
+    # Now run country-dependent options:
+    if myC == 'SL' or myC == 'MW' or myC == 'BO': return hh_df.eval('c/12.').to_frame(name='precautionary_savings')
     if myC == 'RO': return pd.read_csv('../intermediate/RO/hh_savings.csv')[['hhid','precautionary_savings']].set_index('hhid')
 
     if myC == 'PH':
-=======
-    # Now run country-dependent options:  # this assumes savings are a months' worth of income
-    if myC == 'SL' or myC == 'MW' or myC == 'BO': return hh_df.eval('c/12.').to_frame(name='precautionary_savings')
-
-    elif myC == 'PH':
->>>>>>> b1618
 
         # LOAD DECILE INFO
         df_decile = pd.read_csv('../intermediate/'+myC+'/hh_rankings.csv')[['hhid','decile']].astype('int')
@@ -46,4 +40,3 @@ def get_hh_savings(myC, econ_unit, pol, fstr=None):
         return hh_df[['precautionary_savings']]
 
     assert(False)
-
